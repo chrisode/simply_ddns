ce35ed278148d3dd3a16e6961ed9b16ff229131f
Christoffer Söderberg <christoffer@delitech.se>
2023-03-31 14:55:08 +0200

    Refactored structure to be able to better maintain the code, also fixed some minor issues

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Simply DDNS
 
-This is a simple application you can use to update your DNS records at simply.com dynamically. 
-When it runs it will check if your domain has changed since the last time it ran and then update it if it has changed. 
+This is a simple application you can use to update your DNS records at simply.com dynamically.
+When it runs it will check if your domain has changed since the last time it ran and then update it if it has changed.
 
-It uses the [simply.com](https://www.simply.com/en/docs/api/) DDNS API to update the records. 
+It uses the [simply.com](https://www.simply.com/en/docs/api/) DDNS API to update the records.
 
 ## Configuration
-Copy config.example.json and name it config.json. Replace account and apikey with your account id and apikey which you can find in the administration section in your simply.com control panel. 
+Copy config.example.json and name it config.json. Replace account and `apikey` with your account id and API Key which you can find in the administration section in your simply.com control panel.
 
 Then add your records and domains that you want to dynamically update. Each domain is added as a key with a list of records for that domain that is should update.
 
@@ -21,7 +21,7 @@ Example
 ```
 
 ## Running
-Once you have created your configuration you can run the container. You can either run it locally using node js or run it with the prebuilt container. 
+Once you have created your configuration you can run the container. You can either run it locally using node js or run it with the prebuilt container.
 
 ### Using node
 ```bash
--- app.js
+++ app.js
@@ -1,83 +1,36 @@
-import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
-import axios from "axios";
 
 import checkIP from "./lib/checkIP.js";
-import Logger from "./lib/logger.js";
+import getConfig from "./lib/getConfig.js";
+import { log, disableLog } from "./lib/logger.js";
+import { updateHostname } from "./lib/simplyApi.js";
 
-let _config, _instance;
+const isImported = process.argv[1] !== fileURLToPath(import.meta.url);
+if (isImported) {
+  disableLog();
+}
 
-const isNotImported = process.argv[1] === fileURLToPath(import.meta.url);
-const logger = new Logger(isNotImported);
-
-async function getAxiosInstance() {
-
-  if (_instance) {
-    return _instance;
-  }
-
+async function updateDomains() {
   const config = await getConfig();
 
-  _instance = axios.create({
-    baseURL: "https://api.simply.com",
-    timeout: 1000,
-    responseType: "text",
-    auth: {
-      username: config.account,
-      password: config.apikey,
-    },
-  });
-
-  return _instance;
-}
-
-async function getConfig() {
-  if (_config) {
-    return _config;
-  }
-  const configFile = await readFile("./config.json");
-  _config = JSON.parse(configFile);
-  return _config;
-}
-
-async function getDomains() {
-  const config = await getConfig();
-  return config.domains;
-}
-
-async function updateAlHostnames() {
-
-  const domains = await getDomains();
-
-  for (const [ domain, hostnames ] of Object.entries(domains)) {
+  for (const [ domain, hostnames ] of Object.entries(config.domains)) {
     hostnames.forEach((hostname) => {
       updateHostname(domain, hostname);
     });
   }
 }
 
-async function updateHostname(domain, hostname) {
-  try {
-    const instance = await getAxiosInstance();
-    await instance.post(`/2/ddns/?domain=${domain}&hostname=${hostname}`);
-    logger.log(`Succesfully updated hostname ${hostname}`);
-  } catch (error) {
-    logger.log(`Failed to update hostname ${hostname}`);
-    logger.error(error);
-  }
-}
-
 async function app() {
   const isNewIP = await checkIP();
   if (!isNewIP) {
-    logger.log("IP has not changed, aborting...");
+    log("IP has not changed, aborting...");
     return;
   }
-  await updateAlHostnames();
+  await updateDomains();
 }
 
-// Only run if called from terminal, not if included
-if (isNotImported) {
+// Only run if called from terminal, not if imported
+if (!isImported) {
   await app();
 }
 
